094970c01ccb767fb881c44585bf4855da93c59f
harutiro <harutiro2027@outlook.jp>
2021-12-28 16:02:44 +0900

    add:URLからAPIを取得しパースできるようにした

--- ApiTest/ViewController.swift
+++ ApiTest/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    struct RecebedMain: Codable{
+        let status:String
+        let mode:String
+        let data:[String]
+    }
+    
+    struct RecebedData: Codable{
+        let name:String
+        let num:Double
+    }
+    
+    var data: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let str :String = "Minecraft".urlEncoded
+        let jsonUrlString = String("http://127.0.0.1:5000/near?str=\(str)")
+        
+        guard let url = URL(string: jsonUrlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {return}
+            do{
+                let str = String(data: data, encoding: .utf8)!
+                print(str)
+                
+                let myblog = try JSONDecoder().decode(RecebedMain.self, from: data)
+                print(myblog.status)
+                print(myblog.mode)
+                print(myblog.data)
+            } catch let jsonError{
+                print("error", jsonError)
+            }
+        }.resume()
+        
     }
+    
+}
 
-
+extension String {
+    
+    var urlEncoded: String {
+        // 半角英数字 + "/?-._~" のキャラクタセットを定義
+        let charset = CharacterSet.alphanumerics.union(.init(charactersIn: "/?-._~"))
+        // 一度すべてのパーセントエンコードを除去(URLデコード)
+        let removed = removingPercentEncoding ?? self
+        // あらためてパーセントエンコードして返す
+        return removed.addingPercentEncoding(withAllowedCharacters: charset) ?? removed
+    }
 }
 
